a58dc17d0bd729db915ab39e210e0eeeed520109
paoyoquieroprogramar <pao_v65@hotmail.com>
2022-05-26 12:01:00 -0300

    mi primer commit-subiendo tp

---
+++ JS/javascript.js
@@ -0,0 +1,45 @@
+
+
+(function () {
+    var formulario = document.getElementsByName("formulario")[0],
+        elementos = formulario.elements,
+        boton = document.getElementById('btn');
+
+    var validarNombre = function (e) {
+        if (formulario.nombre.value == 0) {
+            alert("completa el campo de nombre");
+            e.preventDefault();
+        }
+    };
+    var validarApellido = function (e) {
+        if (formulario.apellido.value == 0) {
+            alert("complete el campo apellido");
+            e.preventDefault();
+        }
+    };
+    var validarMail = function (e) {
+        if (formulario.mail.value == 0) {
+            alert("complete el campo mail");
+            e.preventDefault();
+        }
+
+    };
+    var validarPago = function (e) {
+        if (!document.querySelector('input[name="pago"]:checked')) {
+            alert("elige un medio de pago");
+            e.preventDefault();
+        }
+    };
+
+
+    var validar = function (e) {
+        validarNombre(e);
+        validarApellido(e);
+        validarMail(e);
+        validarPago(e);
+    };
+    formulario.addEventListener("submit", validar);
+
+}())
+
+
