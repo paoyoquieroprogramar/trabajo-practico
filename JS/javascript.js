41a947c1fc880e4aa952b01e1796ce8018827a73
paoyoquieroprogramar <pao_v65@hotmail.com>
2022-06-02 15:22:55 -0300

    dar formato al archivo js

--- JS/javascript.js
+++ JS/javascript.js
@@ -5,26 +5,26 @@
         elementos = formulario.elements,
         boton = document.getElementById('btn');
 
-    var validarNombre = function (e) {
+    var validarNombre = function(e) {
         if (formulario.nombre.value == 0) {
             alert("completa el campo de nombre");
             e.preventDefault();
         }
     };
-    var validarApellido = function (e) {
+    var validarApellido = function(e) {
         if (formulario.apellido.value == 0) {
             alert("complete el campo apellido");
             e.preventDefault();
         }
     };
-    var validarMail = function (e) {
+    var validarMail = function(e) {
         if (formulario.mail.value == 0) {
             alert("complete el campo mail");
             e.preventDefault();
         }
 
     };
-    var validarPago = function (e) {
+    var validarPago = function(e) {
         if (!document.querySelector('input[name="pago"]:checked')) {
             alert("elige un medio de pago");
             e.preventDefault();
@@ -32,7 +32,7 @@
     };
 
 
-    var validar = function (e) {
+    var validar = function(e) {
         validarNombre(e);
         validarApellido(e);
         validarMail(e);
